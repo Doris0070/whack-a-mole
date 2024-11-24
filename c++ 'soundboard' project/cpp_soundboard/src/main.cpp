46b6311780c8aaef4a39b9180f8a5b8f592aa7bc
Doris0070 <dorianmahnic@gmail.com>
2024-11-24 08:52:57 +0100

    starting prototype 1

---
+++ c++ 'soundboard' project/cpp_soundboard/src/main.cpp
@@ -0,0 +1,17 @@
+#include <Arduino.h>
+#define start_button 4
+#define stop_button 5
+#define button 6
+#define audio_output 7
+#define pot_audio 15
+#define half_adc 128
+#define max_adc 256
+
+void setup(){
+
+}
+
+void loop(){
+
+}
+
